7ed5dd6e38ab890cb2c29d2db7f6c199a734d325
Kostyukovich_V_A <demon4ik228228@gmail.com>
2022-02-02 01:36:04 +0300

    [Task 0.2] Make news catalog aware

--- bin/custom/concerttours/src/de/hybris/training/events/BandAlbumSalesEventListener.java
+++ bin/custom/concerttours/src/de/hybris/training/events/BandAlbumSalesEventListener.java
@@ -1,5 +1,7 @@
 package de.hybris.training.events;
 
+import de.hybris.platform.catalog.CatalogVersionService;
+import de.hybris.platform.catalog.model.CatalogVersionModel;
 import de.hybris.platform.servicelayer.event.impl.AbstractEventListener;
 import de.hybris.platform.servicelayer.model.ModelService;
 import de.hybris.training.model.NewsModel;
@@ -10,8 +12,11 @@ public class BandAlbumSalesEventListener extends AbstractEventListener<BandAlbum
 {
     private static final String BAND_SALES_HEADLINE = "%s album sales exceed 50000";
     private static final String BAND_SALES_CONTENT = "%s album sales reported as %d";
+    private static final String CATALOG_ID = "concertoursProductCatalog";
+    private static final String VERSION_NAME = "Online";
 
     private ModelService modelService;
+    private CatalogVersionService catalogVersionService;
 
     public ModelService getModelService()
     {
@@ -23,6 +28,16 @@ public class BandAlbumSalesEventListener extends AbstractEventListener<BandAlbum
         this.modelService = modelService;
     }
 
+    public CatalogVersionService getCatalogVersionService()
+    {
+        return catalogVersionService;
+    }
+
+    public void setCatalogVersionService(CatalogVersionService catalogVersionService)
+    {
+        this.catalogVersionService = catalogVersionService;
+    }
+
     @Override
     protected void onEvent(final BandAlbumSalesEvent event)
     {
@@ -31,10 +46,12 @@ public class BandAlbumSalesEventListener extends AbstractEventListener<BandAlbum
             final String headline = String.format(BAND_SALES_HEADLINE, event.getName());
             final String content = String.format(BAND_SALES_CONTENT, event.getName(), event.getSales());
             final NewsModel news = modelService.create(NewsModel.class);
+            final CatalogVersionModel catalogVersion = catalogVersionService.getCatalogVersion(CATALOG_ID, VERSION_NAME);
 
             news.setDate(new Date());
             news.setHeadline(headline);
             news.setContent(content);
+            news.setCatalogVersion(catalogVersion);
 
             modelService.save(news);
         }
